4626ac17fd7a651e10ed97ab15c0740f3b010dbc
Jinfeng Lin <linjinfeng@gmail.com>
2012-11-26 11:13:06 +0800

    linjf102

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linjf102::Application.config.secret_token = '56de923926f3bb895bd92b81dc89a8e1cb2805e6bb9dfa5b8d65ece27bc0acdc45523039bf2ce4c83828c5ad2cea175f3395fa2bbeabcc4cd0c3688a62ff2672'
